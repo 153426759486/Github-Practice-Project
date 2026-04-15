8b2378c037cc16d6365610e8f3abef7c54e29257
amang <gauraman411@gmail.com>
2026-04-15 16:02:26 +0530

    Initial Commit

---
+++ src/main/java/com/github/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.github.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/index")
+    public String indexPage(){
+        System.out.println("index is loading!");
+        return "index";
+    }
+}
